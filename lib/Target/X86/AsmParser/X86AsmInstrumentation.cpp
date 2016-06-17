be6571a28bf1cab8c49fe28c544f4303a8cc2dda
Wei Wang <wang0109@gmail.com>
2016-06-17 18:53:56 -0400

    missed one place, test 2TB again

--- lib/Target/X86/AsmParser/X86AsmInstrumentation.cpp
+++ lib/Target/X86/AsmParser/X86AsmInstrumentation.cpp
@@ -752,7 +752,8 @@ void X86AddressSanitizer32::InstrumentMOVSImpl(unsigned AccessSize,
 class X86AddressSanitizer64 : public X86AddressSanitizer {
 public:
   //static const long kShadowOffset = 0x7fff8000;
-  static const long kShadowOffset = 0x2000'0000'0000;
+  //FIXME: testing 2 TB again
+  static const long kShadowOffset = 0x200'0000'0000;  // 32TB.
 
   X86AddressSanitizer64(const MCSubtargetInfo *&STI)
       : X86AddressSanitizer(STI) {}
